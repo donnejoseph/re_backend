7ec70764d21efd202bbdedd471c6359a928d5002
Dinar Yusupov <dinar.yusupov@talent500.co>
2023-10-08 18:52:38 +0300

    test cases for models

--- app/api/tests/test_models.py
+++ app/api/tests/test_models.py
@@ -1,52 +1,127 @@
 from django.test import TestCase
-
 from api.models import City, Area, Property, Image
+from decimal import Decimal
+from django.core.files.uploadedfile import SimpleUploadedFile
+import io
+from PIL import Image as PILImage
+
+
+class CityModelTestCase(TestCase):
+    def setUp(self):
+        self.city = City.objects.create(name="Test City", population="1000000")
+
+    def test_city_creation(self):
+        self.assertEqual(self.city.name, "Test City")
+        self.assertEqual(self.city.population, "1000000")
+
+
+class AreaModelTestCase(TestCase):
+    def setUp(self):
+        self.city = City.objects.create(name="Test City", population="1000000")
+        self.area = Area.objects.create(name="Test Area", city=self.city)
+
+    def test_area_creation(self):
+        self.assertEqual(self.area.name, "Test Area")
+        self.assertEqual(self.area.city, self.city)
 
 
 class PropertyModelTestCase(TestCase):
     def setUp(self):
-        self.city = City.objects.create(name="Test City", population=100000)
-        self.area = Area.objects.create(name="Test Area", city=self.city)
+        self.city = City.objects.create(name="New York", population="8000000")
+        self.area = Area.objects.create(name="Manhattan", city=self.city)
         self.property = Property.objects.create(
             title="Test Property",
-            address="123 Test St",
-            state="Test State",
+            address="123 Main St",
+            state="NY",
             area=self.area,
-            description="Test Description",
             sale_type=Property.SaleType.FOR_SALE,
-            price=100000,
+            price=Decimal('1000000'),
             bedrooms=3,
             bathrooms=2,
             home_type=Property.HomeType.APARTMENT,
             square_fit=1500,
-            object_id="12345",
+            object_id="123456",
+            is_published=True,
+            building_year="2000",
             best_offer=False,
-            modx_id="54321",
+            modx_id="789012",
         )
-        self.image = Image.objects.create(title="Test Image", property=self.property)
+        self.property.city.set([self.city])
 
-    def test_property_model(self):
-        property = Property.objects.get(title="Test Property")
-        self.assertEqual(property.title, "Test Property")
-        self.assertEqual(property.city.count(), 1)
-        self.assertEqual(property.city.first().name, "Test City")
-        self.assertEqual(property.area.name, "Test Area")
-        self.assertEqual(property.images.count(), 1)
+    def test_property_creation(self):
+        self.assertEqual(self.property.title, "Test Property")
+        self.assertEqual(self.property.address, "123 Main St")
+        self.assertEqual(self.property.state, "NY")
+        self.assertEqual(self.property.area, self.area)
+        self.assertEqual(self.property.sale_type, Property.SaleType.FOR_SALE)
+        self.assertEqual(self.property.price, Decimal('1000000'))
+        self.assertEqual(self.property.bedrooms, 3)
+        self.assertEqual(self.property.bathrooms, 2)
+        self.assertEqual(self.property.home_type, Property.HomeType.APARTMENT)
+        self.assertEqual(self.property.square_fit, 1500)
+        self.assertEqual(self.property.object_id, "123456")
+        self.assertEqual(self.property.is_published, True)
+        self.assertEqual(self.property.building_year, "2000")
+        self.assertEqual(self.property.best_offer, False)
 
-    def test_image_model(self):
-        image = Image.objects.get(title="Test Image")
-        self.assertEqual(image.title, "Test Image")
-        self.assertEqual(image.property.title, "Test Property")
+    def test_city_relation(self):
+        """
+        Test the relation between Property and City model.
+        """
+        self.assertEqual(self.property.city.first().name, self.city.name)
 
-class CityModelTestCase(TestCase):
-    def test_city_model(self):
-        city = City.objects.create(name="Test City", population=100000)
-        self.assertEqual(city.name, "Test City")
-        self.assertEqual(city.population, "100000")
+    def test_area_relation(self):
+        """
+        Test the relation between Property and Area model.
+        """
+        self.assertEqual(self.property.area.name, self.area.name)
 
-class AreaModelTestCase(TestCase):
-    def test_area_model(self):
-        city = City.objects.create(name="Test City", population=100000)
-        area = Area.objects.create(name="Test Area", city=city)
-        self.assertEqual(area.name, "Test Area")
-        self.assertEqual(area.city.name, "Test City")
+    def test_string_representation(self):
+        """
+        Test the string representation of Property model instance.
+        """
+        self.assertEqual(str(self.property), "Test Property")
+
+
+class ImageModelTestCase(TestCase):
+    def setUp(self):
+        self.city = City.objects.create(name="New York", population="8000000")
+        self.area = Area.objects.create(name="Manhattan", city=self.city)
+        self.property = Property.objects.create(
+            title="Test Property",
+            address="123 Main St",
+            state="NY",
+            area=self.area,
+            sale_type=Property.SaleType.FOR_SALE,
+            price=1000000,
+            bedrooms=3,
+            bathrooms=2,
+            home_type=Property.HomeType.APARTMENT,
+            square_fit=1500,
+            object_id="123456",
+            is_published=True,
+            building_year="2000",
+            best_offer=False,
+            modx_id="789012",
+        )
+        self.property.city.set([self.city])
+        image = PILImage.new('RGB', (100, 100))
+        file = io.BytesIO()
+        image.save(file, 'JPEG')
+        file.name = 'test.jpg'
+        file.seek(0)
+        uploaded_image = SimpleUploadedFile(file.name, file.read(), content_type='image/jpeg')
+
+        self.image = Image.objects.create(
+            title="Test Image",
+            image=uploaded_image,
+            property=self.property
+        )
+
+    def test_image_creation(self):
+        self.assertEqual(self.image.title, "Test Image")
+        self.assertEqual(self.image.property, self.property)
+        self.assertIn('test.jpg', self.image.image.name)
+
+    def test_property_relation(self):
+        self.assertEqual(self.image.property.title, "Test Property")
--- app/config/settings.py
+++ app/config/settings.py
@@ -119,7 +119,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     Path(BASE_DIR, 'static')
+# ]
+STATICFILES_DIRS = [Path(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR, 'media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
